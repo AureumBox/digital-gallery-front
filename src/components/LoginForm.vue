dd3a461ad7d7b3c13dc58a7de55a7d0bc03f7717
AureumBox <aurimart.liliana@gmail.com>
2025-01-10 07:52:01 -0400

    init

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { Form } from '@primevue/forms';
+import { Button, InputText, Message } from 'primevue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    components: {
+        Form,
+        Button,
+        InputText,
+    },
+})
+</script>
+
+<template>
+    <p>sdfg</p>
+    <Form>
+        <div>
+            <InputText name="username" type="text" placeholder="Username" />
+        </div>
+        <div>
+            <InputText name="email" type="text" placeholder="Email" />
+        </div>
+        <Button type="submit" label="Submit" />
+    </Form>
+
+</template>
---
+++ src/main.ts
@@ -0,0 +1,76 @@
+import './assets/main.css'
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { definePreset } from '@primevue/themes'
+
+const app = createApp(App)
+
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{violet.50}',
+      100: '{violet.100}',
+      200: '{violet.200}',
+      300: '{violet.300}',
+      400: '{violet.400}',
+      500: '{violet.500}',
+      600: '{violet.600}',
+      700: '{violet.700}',
+      800: '{violet.800}',
+      900: '{violet.900}',
+      950: '{violet.950}',
+    },
+    colorScheme: {
+      light: {
+        surface: {
+          0: '#ffffff',
+          50: '{slate.50}',
+          100: '{slate.100}',
+          200: '{slate.200}',
+          300: '{slate.300}',
+          400: '{slate.400}',
+          500: '{slate.500}',
+          600: '{slate.600}',
+          700: '{slate.700}',
+          800: '{slate.800}',
+          900: '{slate.900}',
+          950: '{slate.950}',
+        },
+      },
+      dark: {
+        surface: {
+          0: '#ffffff',
+          50: '{slate.50}',
+          100: '{slate.100}',
+          200: '{slate.200}',
+          300: '{slate.300}',
+          400: '{slate.400}',
+          500: '{slate.500}',
+          600: '{slate.600}',
+          700: '{slate.700}',
+          800: '{slate.800}',
+          900: '{slate.900}',
+          950: '{slate.950}',
+        },
+      },
+    },
+  },
+})
+
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset,
+    options: {
+      prefix: 'p',
+      darkModeSelector: '.app-dark',
+      cssLayer: false,
+    },
+  },
+})
+app.use(router)
+
+app.mount('#app')

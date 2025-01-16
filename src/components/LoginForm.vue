9114d870c29f7bcd493626e60bf77f04722de99e
AureumBox <aurimart.liliana@gmail.com>
2025-01-16 09:09:54 -0400

    bad but working forms

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import router from '@/router';
 import { Form } from '@primevue/forms';
-import { Button, InputText, Message } from 'primevue';
+import { Button, InputText } from 'primevue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -11,23 +11,47 @@ export default defineComponent({
         InputText,
     },
     methods: {
-        onFormSubmit() {
-            console.log("ki")
-            router.push('/')
+        async onFormSubmit() {
+            const formData = {
+                email: (this.$refs.email as HTMLInputElement).value,
+                password: (this.$refs.password as HTMLInputElement).value
+            }
+
+            try {
+                console.log(import.meta.env.VUE_APP_BACKEND)
+                const response = await fetch('https://6788e7492c874e66b7d6ca18.mockapi.io/api/signup', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formData),
+                });
+
+                if (!response.ok) {
+                    throw new Error('Error en la red');
+                }
+
+                const data = await response.json();
+                console.log("Respuesta del servidor:", data);
+                alert("Registrado")
+                await router.push('/home');
+                window.location.reload()
+            } catch (error) {
+                console.error("Error al enviar el formulario:", error);
+            }
         }
     }
-})
-
+});
 </script>
 
 <template>
     <div class="form-container">
-        <Form @submit.stop.prevent="onFormSubmit">
+        <Form @submit="onFormSubmit">
             <div class="form-field">
-                <InputText name="username" type="text" placeholder="Username" />
+                <InputText name="email" type="email" placeholder="Email" ref="email" />
             </div>
             <div class="form-field">
-                <InputText name="email" type="email" placeholder="Email" />
+                <InputText name="password" type="password" placeholder="Password" ref="password" />
             </div>
             <Button type="submit" label="Submit" class="submit-button" />
         </Form>
